a183686501e61b3a0f5ae45043be6a2a9381bb9f
Nick Anthony Miras <mirasnickanthony@gmail.com>
2026-02-11 19:49:09 +0800

    new stuff

--- deployment/README.md
+++ deployment/README.md
@@ -4,3 +4,10 @@ Terraform configuration for fast deployment.
 ## Prerequisites
 You need to download the following cli tools to do everything here
 - [Terraform](https://www.terraform.io/downloads)
+
+
+## Terminating Resources
+If you are done with training your model, you can terminate the resources you provisioned by:
+```bash
+terraform destroy
+```
---
+++ iris/train.py
@@ -0,0 +1,79 @@
+# import libraries
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib_inline.backend_inline
+from ucimlrepo import fetch_ucirepo
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+
+matplotlib_inline.backend_inline.set_matplotlib_formats('svg')
+
+
+def get_data():
+    return fetch_ucirepo(id=53)
+
+
+def preprocess_data(dataset):
+    scaler = MinMaxScaler()
+
+    # set x variables
+    X = dataset.data.features
+    X = pd.DataFrame(scaler.fit_transform(X), columns=X.columns)
+
+    # set y variables
+    y = dataset.data.targets
+    y = y.copy()
+    y["class"] = y["class"].replace({'Iris-setosa':0,'Iris-versicolor':1,'Iris-virginica':2})
+
+    data = torch.tensor(X.values).float()
+    labels = torch.tensor(y.values).long().squeeze()
+    return data, labels
+
+
+def get_model():
+    return nn.Sequential(
+        nn.ReLU(),         # activation
+        nn.Linear(64,64),  # hidden layer
+        nn.ReLU(),         # activation
+        nn.Linear(64,64),  # hidden layer
+        nn.ReLU(),         # activation
+        nn.Linear(64,3),   # output layer
+    )
+
+
+def train_model(model, data, labels, epochs=1000):
+    losses = torch.zeros(epochs)
+
+    model = get_model()
+    lossfunc = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+    accuracies = []
+
+    # loop over epochs
+    for i in range(epochs):
+
+        # forward pass
+        pred = model(data)
+
+        # compute loss
+        loss = lossfunc(pred,labels)
+        losses[i] = loss
+
+        # backprop
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # compute accuracy
+        matches = torch.argmax(pred,axis=1) == labels # booleans (false/true)
+        matchesNumeric = matches.float()              # convert to numbers (0/1)
+        accuracyPct = 100*torch.mean(matchesNumeric)  # average and x100
+        accuracies.append( accuracyPct )              # add to list of accuracies
+
+    predictions = model(data)
+
